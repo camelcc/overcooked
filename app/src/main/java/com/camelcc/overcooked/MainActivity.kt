7e6062ccf6acd10eb5c5ab272ce362e62993fff9
camelcc <camel.young@gmail.com>
2022-04-09 01:41:15 -0700

    setup:

---
+++ app/src/main/java/com/camelcc/overcooked/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.camelcc.overcooked
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
